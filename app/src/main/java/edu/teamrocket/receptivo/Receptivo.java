97ce35f10c9515f8f0c3e081023f1277740705ff
MiguelPahino <mpahinogutierrez@gmail.com>
2026-05-11 22:25:06 +0200

    feat(Receptivo): :sparkles: Añadir el módulo Receptivo

---
+++ app/src/main/java/edu/teamrocket/receptivo/Receptivo.java
@@ -0,0 +1,28 @@
+package edu.teamrocket.receptivo;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import edu.teamrocket.payment.CreditCard;
+
+public class Receptivo implements GuestDispatcher {
+
+    private static Receptivo instance = null;
+    
+    private final Set<GuestDispatcher> registrados = new LinkedHashSet<>();
+
+    private Receptivo(){};
+
+    public static Receptivo getReceptivo(){
+        return  instance == null ? instance = new Receptivo() : instance;
+    }
+
+    public void registrar(GuestDispatcher registrado){
+        this.registrados.add(registrado);
+    }
+
+    @Override
+    public void dispatch(CreditCard card) {
+        this.registrados.stream().forEach(x-> x.dispatch(card));
+    }
+}
