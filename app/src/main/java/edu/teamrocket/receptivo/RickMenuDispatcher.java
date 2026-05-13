28db4aa36d81672da92c4e65e1a413e7e9b06a96
MiguelPahino <mpahinogutierrez@gmail.com>
2026-05-13 23:39:02 +0200

    feat: :sparkles: Añadir el módulo de RickMenuDispatcher

---
+++ app/src/main/java/edu/teamrocket/receptivo/RickMenuDispatcher.java
@@ -0,0 +1,42 @@
+package edu.teamrocket.receptivo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.teamrocket.payment.CreditCard;
+
+public class RickMenuDispatcher implements GuestDispatcher {
+    
+    private int stock = 100;
+    private double price = 10d;
+    private List<CreditCard> invitados = new ArrayList<>();
+
+
+    @Override
+    public void dispatch(CreditCard card) {
+        if(stock > 0){
+            card.pay(price);
+            stock --;
+        }
+    }
+
+    
+
+    public int stock() {
+        return stock;
+    }
+
+
+
+    @Override
+    public String toString() {
+        StringBuilder build =new StringBuilder();
+        invitados.stream()
+        .forEach(x-> {
+            build.append(x.cardOwner());
+            build.append("\n");
+        });
+
+        return build.toString();
+    }
+}
