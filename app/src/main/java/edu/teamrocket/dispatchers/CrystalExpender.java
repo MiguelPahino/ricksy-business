99d894e1320c6f84370af1cbd73b9d6400d1602d
MiguelPahino <mpahinogutierrez@gmail.com>
2026-05-13 23:38:33 +0200

    feat: :sparkles: Añadir el módulo de CrystalExpender

---
+++ app/src/main/java/edu/teamrocket/dispatchers/CrystalExpender.java
@@ -0,0 +1,41 @@
+package edu.teamrocket.dispatchers;
+
+import edu.teamrocket.payment.CreditCard;
+import edu.teamrocket.receptivo.GuestDispatcher;
+
+public class CrystalExpender implements GuestDispatcher{
+
+
+    private int stock = 0;
+    private double itemCost = 0d;
+
+    public CrystalExpender(int stock, double itemCost){
+        this.stock = stock;
+        this.itemCost = itemCost;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder build = new StringBuilder();
+
+        build.append("Stock: ");
+        build.append(stock);
+        build.append("\nCost: ");
+        build.append(itemCost);
+
+        return build.toString();
+    }
+
+    public int stock() {
+        return stock;
+    }
+
+    @Override
+    public void dispatch(CreditCard card) {
+        if(stock > 0){
+            card.pay(itemCost);
+            stock --;
+        }
+    }
+    
+}
